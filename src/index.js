30a78398802f346334f46f1c70158046eed0496d
Cristian Caroli <cristiancaroli@Cristians-MacBook-Pro.local>
2017-06-28 15:15:51 +0200

    small change

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import express from 'express'
 
 express()
   .use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-    )
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Content-Type, Authorization, Content-Length, X-Requested-With'
-    )
+    res
+      .header('Access-Control-Allow-Origin', '*')
+      .header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+      )
+      .header(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization, Content-Length, X-Requested-With'
+      )
     next()
   })
   .get('/person/:name', getActorName)
