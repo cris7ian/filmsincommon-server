f1caaaecf2603681c4033c1ac316391d5f2a3b18
Cristian Caroli <cristiancaroli@Cristians-MacBook-Pro.local>
2017-07-01 12:39:25 +0200

    redis

--- src/api.js
+++ src/api.js
@@ -5,9 +5,7 @@ import sortBy from 'lodash/sortBy'
 import uniqBy from 'lodash/uniqBy'
 import { createResults } from './helpers'
 import async from 'async'
-import mdbAPI from 'moviedb'
-
-const mdb = mdbAPI(process.env['MDB_API_KEY'])
+import { search } from './datasource'
 
 const PROFILE_PICS_URL = 'http://image.tmdb.org/t/p/w45'
 
@@ -20,14 +18,11 @@ const getActorName = (req, res) => {
   }
   console.log(`query for name "${name}"`)
   //we use 'ngram' because we'll want to use it as an autocomplete method (see API).
-  mdb.searchPerson(
+  search(
     { query: name, search_type: 'ngram', include_adult: true },
-    (err1, personInfo) => {
-      if (err1) {
-        return res
-          .status(500)
-          .send({ error: 'Could not find person. API error' })
-      }
+    'searchPerson'
+  )
+    .then(personInfo => {
       if (isEmpty(personInfo.results)) {
         return res.json([])
       }
@@ -42,8 +37,10 @@ const getActorName = (req, res) => {
         return obj.popularity
       }).reverse()
       return res.json(personInfo.results)
-    }
-  )
+    })
+    .catch(() =>
+      res.status(500).send({ error: 'Could not find person. API error' })
+    )
 }
 
 const getActorRevenue = (req, res) => {
@@ -53,21 +50,17 @@ const getActorRevenue = (req, res) => {
     return res.status(500).send({ error: 'Need an id.' })
   }
   console.log(`query for revenue of ${id}`)
-  try {
-    const getRevenue = (credit, callback) => {
-      mdb.movieInfo({ id: credit.id }, (err, result) => {
-        if (err) {
-          return callback(err, 0)
-        }
-        credit.revenue = result.revenue
-        callback(null, result.revenue)
-      })
-    }
+  const getRevenue = (credit, callback) =>
+    search({ id: credit.id }, 'movieInfo')
+      .then(result => callback(null, result.revenue))
+      .catch(err => callback(err, 0))
 
-    mdb.personCredits({ id: id, include_adult: true }, (err2, credits) => {
-      if (err2 || !credits.cast) {
+  search({ id, include_adult: true }, 'personCredits')
+    .then(credits => {
+      if (!credits.cast) {
         throw new Error('API LIMIT ERROR')
       }
+
       async.map(credits.cast, getRevenue, (err, results) => {
         if (err) {
           return res.status(500).send({ error: 'error collecting info.' })
@@ -76,19 +69,16 @@ const getActorRevenue = (req, res) => {
         return res.json({ revenue: revenue, credits: credits })
       })
     })
-  } catch (error) {
-    return res.status(500).send({ error: 'API error getting revenue.' })
-  }
+    .catch(() => res.status(500).send({ error: 'API error getting revenue.' }))
 }
 
 const getCredits = id =>
   new Promise((resolve, reject) => {
-    mdb.personCredits({ id, include_adult: true }, (err, credits) => {
-      if (err) {
-        return reject('error getting credits')
-      }
-      return resolve(uniqBy([...credits.cast, ...credits.crew], 'title'))
-    })
+    search({ id, include_adult: true }, 'personCredits')
+      .then(credits =>
+        resolve(uniqBy([...credits.cast, ...credits.crew], 'title'))
+      )
+      .catch(() => reject('error getting credits'))
   })
 
 const getConnection = (req, res) => {
---
+++ src/datasource.js
@@ -0,0 +1,40 @@
+import mdbAPI from 'moviedb'
+import redis from 'redis'
+import bluebird from 'bluebird'
+
+bluebird.promisifyAll(redis.RedisClient.prototype)
+bluebird.promisifyAll(redis.Multi.prototype)
+
+const client = redis.createClient(process.env.REDIS_URL)
+
+const mdb = mdbAPI(process.env['MDB_API_KEY'])
+
+const TWO_DAYS = 60 * 60 * 24 * 2
+
+const searchInMBD = (options, method) =>
+  new Promise((resolve, reject) =>
+    mdb[method](options, (err, info) => {
+      if (err) {
+        reject()
+      } else {
+        client.set(
+          `${method}:${options.query || options.id}`,
+          JSON.stringify(info),
+          'EX',
+          TWO_DAYS
+        )
+        resolve(info)
+      }
+    })
+  )
+
+export const search = (options, method) => {
+  return client
+    .getAsync(`${method}:${options.query || options.id}`)
+    .then(
+      result => (result ? JSON.parse(result) : searchInMBD(options, method))
+    )
+    .catch(error => {
+      console.log(`error!: ${error}`)
+    })
+}
